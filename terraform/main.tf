c1fd633198a7eb759d7ca5febc0cd4e742674bd9
rafael <rafael@gmail.com>
2025-03-02 00:58:16 +0200

    Moved terraform file from the Terraform repo to this to intigrate it with the Jenkins file

---
+++ terraform/main.tf
@@ -0,0 +1,68 @@
+terraform {
+  backend "remote" {
+    organization = "rsr-projects"
+    workspaces {
+      name = "rsr-stocknewsapplication"
+    }
+    
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.89.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0c55b159cbfafe1f0" # Ubuntu 20.04
+  instance_type = "t2.micro"
+  key_name = "rsrkey"
+  security_groups = [aws_security_group.rsr-sg.name]
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update -y
+              sudo apt-get install docker.io -y
+              systemctl start docker
+              systemctl enable docker
+              sudo docker pull rsrprojects/flask-news-app:test
+              sudo docker run -d -p 5000:5000 rsrprojects/flask-news-app:test
+              EOF
+  tags = {
+    Name = "news-web-app"
+  }
+}
+
+resource "aws_security_group" "rsr-sg" {
+  name        = "rsr-sg"
+  description = "Allow inbound traffic"
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.app_server.public_ip
+}
